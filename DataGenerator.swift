2b7a514f3f6d0928eefc6c1276b161875c6ee2ed
Prathap Reddy <58396526+Prathap-iOS@users.noreply.github.com>
2020-09-21 12:58:38 +0530

    Add files via upload

---
+++ DataGenerator.swift
@@ -0,0 +1,32 @@
+//
+//  DataGenerator.swift
+//  SampleCharts
+//
+//  Created by Prathap Reddy on 17/09/20.
+//
+
+import Foundation
+
+struct Sale {
+    var month: String
+    var value: Double
+}
+
+class DataGenerator {
+    
+    static var randomizedSale: Double {
+        return Double(arc4random_uniform(10000) + 1) / 10
+    }
+    
+    static func data() -> [Sale] {
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        var sales = [Sale]()
+        
+        for month in months {
+            let sale = Sale(month: month, value: randomizedSale)
+            sales.append(sale)
+        }
+        
+        return sales
+    }
+}
